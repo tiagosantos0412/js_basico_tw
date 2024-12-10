7c94e59a5e95105865d18be9ae5a5177859111ec
Tiago <tiagosantos0412@gmail.com>
2024-12-10 17:07:44 -0300

    Objetos em JS

---
+++ js/objeto.js
@@ -0,0 +1,26 @@
+/**
+ * Criando um objeto literal - diretamente no código
+ */
+
+var aluno = {
+    nome: 'Tiago',
+    idade: 38,
+    ra: '236655',
+    statusMatricula: false,
+    ferias: true,
+    estudar: ()=>{
+        console.log('A pessoa está estudando!')
+    },
+    condicao: ()=> {
+        if(aluno.statusMatricula === true && aluno.ferias === false){
+            console.log(`O aluno ${aluno.nome} está no ano letivo!`)
+        }else if(aluno.ferias === true && aluno.statusMatricula === true){
+            console.log(`O aluno ${aluno.nome}, está de férias`)
+        }else {
+            console.log(`O aluno ${aluno.nome}, não está matriculado no período letivo!`)
+        }
+    }
+
+}
+
+console.log(aluno.condicao())
