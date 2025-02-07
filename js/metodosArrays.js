a8cfc80da137a7a52b5c6a5a747c246151276f6e
Tiago <tiagosantos0412@gmail.com>
2025-02-07 09:30:32 -0300

    Metodos de arrays.

--- js/metodosArrays.js
+++ js/metodosArrays.js
@@ -18,3 +18,19 @@ console.log(meuArray);
 
 meuArray.pop(4);
 console.log(meuArray);
+
+meuArray.unshift('teste');
+console.log(meuArray);
+
+meuArray.shift();
+console.log(meuArray);
+
+meuArray2.splice(0,1);
+console.log(meuArray2);
+
+meuArray2.splice(1, 0, 'teste');
+console.log(meuArray2);
+
+var meuArray3 = [1,2,3,4,5,6];
+meuArray3 = meuArray3.slice(2, 5);
+console.log(meuArray3);
