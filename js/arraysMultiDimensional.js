9d47850ba77aa0cfa7c9d7655de75bfbfc402c27
Tiago <tiagosantos0412@gmail.com>
2025-02-16 10:11:10 -0300

    Introdução array multidimensional ou matriz.

---
+++ js/arraysMultiDimensional.js
@@ -0,0 +1,13 @@
+var matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+console.log(matriz[1][0]);
+
+for(let i = 0; i < matriz.length; i ++){
+    for(let j = 0; j < matriz.length; j++){
+        console.log(matriz[i][j]);
+    }
+}
